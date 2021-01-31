1b57357c885302fe7316d6bf4801875c894dc924
Matthew D. Sun <matthewsun98@gmail.com>
2021-01-31 13:17:07 -0500

    Minor improvements so script runs without breaking

--- structural_virality/create_graphs.py
+++ structural_virality/create_graphs.py
@@ -1,13 +1,23 @@
 from scipy.sparse import csr_matrix, save_npz
+import networkx as nx
 import pickle as pkl
 import os
+import datetime
 
 from graph_utils import calc_avg_degree, implied_beta, scale_free_graph
 
 num_nodes = 1000000
 graph_dir = "graphs_1m"
+num_graphs = 100
+start_ind = 0
+r = 0.5
 
-for i in range(75):
+# create graphs directory if one does not exist
+if not os.path.exists(graph_dir):
+    os.mkdir(graph_dir)
+
+# iteratively create graphs
+for i in range(start_ind, start_ind + num_graphs):
     param_dict = {}
     subdir = f"{graph_dir}/{i}"
     if not os.path.exists(subdir):
@@ -17,10 +27,10 @@ for i in range(75):
     user_rep = nx.convert_matrix.to_scipy_sparse_matrix(G) # convert to scipy adjacency matrix
     save_npz(f'{graph_dir}/{i}/sparse_matrix.npz', user_rep)
     param_dict["k"] = calc_avg_degree(G)
-    param_dict["beta"] = implied_beta(k, r)
+    param_dict["r"] = r
+    param_dict["beta"] = implied_beta(param_dict["k"], r)
     param_dict["num_nodes"] = num_nodes
     f = open(f'{graph_dir}/{i}/param.pkl', 'wb')
     pkl.dump(param_dict, f, -1)
     f.close()
-    break
     
